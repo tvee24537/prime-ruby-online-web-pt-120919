4acf1318dd194138ce3fc55ff4b528c6c645cbb8
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2019-12-21 04:06:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,6 +5,6 @@ def prime?(numbers)
   elsif numbers == 2 #if number is 2 then it's prime
     return true
   else 
-    (2..numbers/2).none? { |x| numbers % x == 0} #if whatever number is divisible by 2 then 
+    (2..numbers/2).none? { |x| numbers % x == 0} #if whatever number is divisible by 2 = to 0 then false
   end
 end
